427ceba64d15ca43948eb22ee4e7957cf1112f2d
osvaldshpengler <ustinov.ant@gmail.com>
2016-04-12 21:52:53 +0300

    base Dockerfile's

---
+++ docker/db/1.sql
@@ -0,0 +1,11 @@
+CREATE ROLE browsercall LOGIN UNENCRYPTED PASSWORD 'browsercall';
+CREATE DATABASE browsercall WITH ENCODING 'UTF8' LC_COLLATE 'ru_RU.utf8'  LC_CTYPE 'ru_RU.utf8' OWNER browsercall TEMPLATE template0;
+
+\c browsercall;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(256),
+  email VARCHAR(256),
+  password VARCHAR(256)
+)
